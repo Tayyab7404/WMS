9eca1a88e5603a7ba24db8d4e6be2aadc39f3d46
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2024-06-27 11:29:42 +0530

    Add files via upload

---
+++ Program 1/DomBookParser.java
@@ -0,0 +1,45 @@
+// Write a program to access an XMLdocument using DOM Parser (Java API):
+
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
+public class DomBookParser
+{
+	public static void main(String[] args)
+	{
+		try
+        	{
+            		System.out.println("\nDOM Parser using Java API\n");
+
+            		DocumentBuilderFactory fact1 = DocumentBuilderFactory.newInstance();
+            		fact1.setValidating(true);
+            		fact1.setIgnoringElementContentWhitespace(true);
+            		DocumentBuilder build1 = fact1.newDocumentBuilder();
+
+            		String book1 = "book.xml";
+            		Document bookdoc = build1.parse(new File(book1));
+            		Element bookele = bookdoc.getDocumentElement();
+            		NodeList chapternodes = bookele.getChildNodes();
+
+            		for(int i=0; i<chapternodes.getLength(); i++)
+            		{
+                		Element chapter = (Element)chapternodes.item(i);
+                
+                		NodeList numberlist = chapter.getElementsByTagName("chapternum");
+                		NodeList namelist = chapter.getElementsByTagName("chaptername");
+
+                		Text number = (Text)numberlist.item(0).getFirstChild();
+                		Text name = (Text)namelist.item(0).getFirstChild();
+                
+                		System.out.println("Tag Name: " + chapter.getNodeName());
+                		System.out.println("\tChapter Number: " + number.getData());
+                		System.out.println("\tChapter Name: " + name.getData() + "\n");
+            		}
+        	}
+        	catch(Exception e)
+        	{
+            		System.err.println(e.getMessage());
+        	}
+	}
+}
